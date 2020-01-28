cbc4efaf04fdafdda5d5a60b1f085054fa1d60b3
hammsidh <hammsidhsetyawan@gmail.com>
2020-01-28 13:10:18 +0700

    get cart and add cart

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const hapi = require('@hapi/hapi')
-const {routes} = require('./src/routes/routes')
+const { routes } = require('./src/routes/routes')
+const Path = require('path')
+const Qs = require('qs')
 
 const start = async () => {
     const server = hapi.server({
         port: process.env.PORT || 3333,
-        host: '0.0.0.0'
+        host: '0.0.0.0',
+        routes: {
+            files: {
+                relativeTo: Path.join(__dirname, 'src/img')
+            }
+        },
+        query: {
+            parser: (query) => Qs.parse(query)
+        }
     })
 
     server.route(routes);
--- src/handlers/product.js
+++ src/handlers/product.js
@@ -1,13 +1,24 @@
 const {
       Product, User
 } = require('../models/mongoSchema')
+const path = require('path')
+const fs = require('fs')
 
 exports.addProduct = async (request, h) => {
       try {
-            const id = Math.floor(Math.random() * (10 ** 16))
-            Object.assign(request.payload, { id: id })
+            let id = Math.floor(Math.random() * (10 ** 16))
+            let extension = path.extname(request.payload.picture.hapi.filename)
+            let timestamp = new Date().getTime();
+            let fileName = email + '-' + timestamp + extension;
+            (request.payload.picture).pipe(fs.createWriteStream(__dirname + '/src/img/' + fileName))
+            Object.assign(request.payload, { id: id, picture: fileName })
+            console.log(request.payload)
             const newProduct = new Product(request.payload)
             await newProduct.save()
+            // await Product.insertMany([{
+            //       id:id,
+            //       email: 
+            // }])
             return h.response('success')
       } catch (err) {
             return h.response('failed').code(401)
@@ -26,7 +37,7 @@ exports.getProduct = async (request, h) => {
 exports.addCart = async (request, h) => {
       const { email } = request.params
       const { payload } = request
-      return User.findOneAndUpdate({ 'email': email }, { $push: { cart: { 'title': payload.title, 'description': payload.description, 'quantity': payload.quantity, 'price': payload.price } } }, { new: true })
+      return User.findOneAndUpdate({ 'email': email }, { $push: { cart: { 'title': payload.title, 'description': payload.description, 'stock': payload.stock, 'price': payload.price, 'seller': payload.seller } } }, { new: true })
             .then(res => {
                   if (!res) {
                         return h.response({ message: "Email not found" })
